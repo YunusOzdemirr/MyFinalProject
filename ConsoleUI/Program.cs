4bfa5f505543e542743b89a4d656e71e94619ddb
Yunus <yunusozdemir468@gmail.com>
2021-02-13 14:22:51 +0300

    Category Entities has been added

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,18 +7,26 @@ namespace ConsoleUI
 {
     //SOLID
     //Open Closed Principle
-    
     class Program
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var item in productManager.GetByUnitPrice(40,100))
+            foreach (var item in productManager.GetByUnitPrice(40, 100))
             {
                 Console.WriteLine(item.ProductName);
             }
             Console.Read();
-
         }
     }
 }
