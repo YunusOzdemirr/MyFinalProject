f2e81ba435f046bd3f26919fa4f9af26bca09b23
Yunus <yunusozdemir468@gmail.com>
2021-02-13 13:25:01 +0300

    Core layer added the project

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public interface EfEntityRepositoryBase<TEntity,TContext>
+    {
+    }
+}
