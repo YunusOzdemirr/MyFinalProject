4736c5cae1f6d92b875e1fa257c0009d4d9be42f
Yunus <yunusozdemir468@gmail.com>
2021-02-15 14:05:20 +0300

    WebAPI layer has been added the project

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //IoC Container -- Inversion of control
+        IProductDal _productDal;
+
+        public ProductsController(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        [HttpGet]
+        public List<Product> Get() 
+        {
+            var result = _productDal.GetAll();
+             return result;
+            //return new IDataResult<<List>Product>(result.Data, Messages.ProductList);
+        }
+    }
+}
+ 
