fe03653f4f0f5c2d78344307d1b5c94810e1a575
Yunus <yunusozdemir468@gmail.com>
2021-04-10 19:42:43 +0300

    True connection string is successfully

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -13,15 +13,17 @@ namespace DataAccess.Concrete.EntityFramework
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
-            optionsBuilder.UseSqlServer(@"Data Source=<localhost>;Initial Catalog=Northwind; Encrypt=True");
+            //for mac
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=Northwind;User=sa;Password=Password123@jkl#;");
+           // optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=Northwind;User=sa;Password=Password123@jkl#; Encrypt=True;");
         }
+
         public DbSet<Product> Products{ get; set; }
         public DbSet<Customer> Customers{ get; set; }
         public DbSet<Category> Categories{ get; set; }
         public DbSet<Order> Orders{ get; set; }
         public DbSet<OperationClaim> OperationClaims{ get; set; }
         public DbSet<User> Users{ get; set; }
-        public DbSet<UserOperationClaim> UserOperationClaims{ get; set; }
-        
+        public DbSet<UserOperationClaim> UserOperationClaims{ get; set; }       
     }
 }
